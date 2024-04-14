25352cbb34ab7d09c3a4976d0fa53dce96ac7687
Hannah Ripley <hriple@gmail.com>
2024-04-14 18:11:33 -0400

    Updated to use client

--- express/utils/queries.js
+++ express/utils/queries.js
@@ -11,263 +11,325 @@ pool.on('error', (err, client) => {
 })
 
 // game queries
-const getGames = (request, response) => {
-  pool.query('SELECT * FROM games ORDER BY id ASC', (error, results) => {
+const getGames = async (request, response) => {
+  const client = await pool.connect()
+  client.query('SELECT * FROM games ORDER BY id ASC', (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(200).json(results.rows)
   })
+  client.release();
 }
 
-const resetGame = (request, response) => {
-  pool.query('UPDATE games SET turn = $1, mode = $2 RETURNING *', [0, 'lobby'], (error, results) => {
+const resetGame = async (request, response) => {
+  const client = await pool.connect()
+  client.query('UPDATE games SET turn = $1, mode = $2 RETURNING *', [0, 'lobby'], (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(201).send(`Reset game with id: ${results.rows[0].id}`)
   })
+  client.release();
 }
 
-const updateGameMode = (request, response) => {
+const updateGameMode = async (request, response) => {
+  const client = await pool.connect()
   const mode = request.params.mode;
-  pool.query('UPDATE games SET mode = $1 RETURNING *;', [mode], (error, results) => {
+  client.query('UPDATE games SET mode = $1 RETURNING *;', [mode], (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(201).send(`Updated game mode to: ${results.rows[0].mode}`)
   })
+  client.release();
 }
 
-const updateTurn = (request, response) => {
+const updateTurn = async (request, response) => {
+  const client = await pool.connect()
   const turnString = request.params.turn;
   const turn = parseInt(turnString);
-  pool.query('UPDATE games SET turn = $1 RETURNING *;', [turn], (error, results) => {
+  client.query('UPDATE games SET turn = $1 RETURNING *;', [turn], (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(201).send(`Updated game turn to: ${results.rows[0].turn}`)
   })
+  client.release();
 }
 
 // card queries
-const getCards = (request, response) => {
-  pool.query('SELECT * FROM cards ORDER BY id ASC', (error, results) => {
+const getCards = async (request, response) => {
+  const client = await pool.connect()
+  client.query('SELECT * FROM cards ORDER BY id ASC', (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(200).json(results.rows)
   })
+  client.release();
 }
 
-const getPlayerCards = (request, response) => {
+const getPlayerCards = async (request, response) => {
+  const client = await pool.connect()
   const player = request.params.player;
-  pool.query('SELECT * FROM cards WHERE player_id = $1 ORDER BY id ASC', [player], (error, results) => {
+  client.query('SELECT * FROM cards WHERE player_id = $1 ORDER BY id ASC', [player], (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(200).json(results.rows)
   })
+  client.release();
 }
 
-const getSolution = (request, response) => {
-  pool.query('SELECT * FROM cards WHERE player_id IS NULL AND type != $1 ORDER BY id ASC', ['none'], (error, results) => {
+const getSolution = async (request, response) => {
+  const client = await pool.connect()
+  client.query('SELECT * FROM cards WHERE player_id IS NULL AND type != $1 ORDER BY id ASC', ['none'], (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(200).json(results.rows)
   })
+  client.release();
 }
 
-const resetCards = (request, response) => {
-  pool.query('UPDATE cards SET player_id = $1 RETURNING *;', [null], (error, results) => {
+const resetCards = async (request, response) => {
+  const client = await pool.connect()
+  client.query('UPDATE cards SET player_id = $1 RETURNING *;', [null], (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(201).send(`Reset all cards`);
   })
+  client.release();
 }
 
-const setCardPlayer = (request, response) => {
+const setCardPlayer = async (request, response) => {
+  const client = await pool.connect()
   const id = request.params.id;
   const player = request.params.player;
-
-  pool.query('UPDATE cards SET player_id = $1 WHERE id = $2 RETURNING *;', [player, id], (error, results) => {
+  client.query('UPDATE cards SET player_id = $1 WHERE id = $2 RETURNING *;', [player, id], (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(201).send(`Updated card with ID: ${results.rows[0].id}`)
   })
+  client.release();
 }
 
 // player queries
 
-const getPlayers = (request, response) => {
-  pool.query('SELECT * FROM players ORDER BY id ASC', (error, results) => {
+const getPlayers = async (request, response) => {
+  const client = await pool.connect()
+  client.query('SELECT * FROM players ORDER BY id ASC', (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(200).json(results.rows)
   })
+  client.release();
 }
 
-const removePlayers = (request, response) => {
-  pool.query('DELETE from players RETURNING *;', [], (error, results) => {
+const removePlayers = async (request, response) => {
+  const client = await pool.connect();
+  client.query('DELETE from players RETURNING *;', [], (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(200).send(`Removed all players`)
   })
+  client.release();
 }
 
-const removePlayer = (request, response) => {
+const removePlayer = async (request, response) => {
   const playerId = request.params.player;
-
-  pool.query('UPDATE players SET canmove = $1 WHERE id = $2 RETURNING *;', [false, playerId], (error, results) => {
+  const client = await pool.connect()
+  client.query('UPDATE players SET canmove = $1 WHERE id = $2 RETURNING *;', [false, playerId], (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(201).send(`Removed player with id: ${results.rows[0].id}`)
   })
+  client.release();
 }
 
-const addPlayer = (request, response) => {
-
+const addPlayer = async (request, response) => {
+  const client = await pool.connect()
   const id = request.params.id;
   const loc = request.params.loc;
   const color = request.params.color;
-
-  pool.query('INSERT INTO players (id, loc, color, cansuggest, canmove) VALUES ($1, $2, $3, $4, $5) RETURNING *', [id, loc, color, false, true], (error, results) => {
+  client.query('INSERT INTO players (id, loc, color, cansuggest, canmove) VALUES ($1, $2, $3, $4, $5) RETURNING *', [id, loc, color, false, true], (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(201).send(`Player added with ID: ${results.rows[0].id}`)
   })
+  client.release();
 }
 
-const updatePlayerLocation = (request, response) => {
-
+const updatePlayerLocation = async (request, response) => {
+  const client = await pool.connect()
   const playerId = request.params.id;
   const loc = request.params.loc;
 
-  pool.query('UPDATE players SET loc = $1 WHERE id = $2 RETURNING *;', [loc, playerId], (error, results) => {
+  client.query('UPDATE players SET loc = $1 WHERE id = $2 RETURNING *;', [loc, playerId], (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(201).send(`Moved player with id: ${results.rows[0].id}`)
   })
+  client.release();
 }
 
-const updateCanSuggest = (request, response) => {
-
+const updateCanSuggest = async (request, response) => {
+  const client = await pool.connect()
   const playerId = request.params.id;
   const canSuggest = request.params.suggest;
 
-  pool.query('UPDATE players SET cansuggest = $1 WHERE id = $2 RETURNING *;', [canSuggest, playerId], (error, results) => {
+  client.query('UPDATE players SET cansuggest = $1 WHERE id = $2 RETURNING *;', [canSuggest, playerId], (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(201).send(`Update player with id: ${results.rows[0].id}`)
   })
+  client.release();
 }
 
-const updateCanMove = (request, response) => {
-
+const updateCanMove = async (request, response) => {
+  const client = await pool.connect()
   const playerId = request.params.id;
-  const canSuggest = request.params.move;
+  const canMove = request.params.move;
 
-  pool.query('UPDATE players SET canmove = $1 WHERE id = $2 RETURNING *;', [canMove, playerId], (error, results) => {
+  client.query('UPDATE players SET canmove = $1 WHERE id = $2 RETURNING *;', [canMove, playerId], (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(201).send(`Update player with id: ${results.rows[0].id}`)
   })
+  client.release();
 }
 
 // weapon queries
-const getWeapons = (request, response) => {
-  pool.query('SELECT * FROM weapons ORDER BY id ASC', (error, results) => {
+const getWeapons = async (request, response) => {
+  const client = await pool.connect()
+  client.query('SELECT * FROM weapons ORDER BY id ASC', (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(200).json(results.rows)
   })
+  client.release();
 }
 
-const updateWeaponLocation = (request, response) => {
-
+const updateWeaponLocation = async (request, response) => {
+  const client = await pool.connect()
   const weaponId = request.params.id;
   const loc = request.params.loc;
 
-  pool.query('UPDATE weapons SET loc = $1 WHERE id = $2 RETURNING *;', [loc, weaponId], (error, results) => {
+  client.query('UPDATE weapons SET loc = $1 WHERE id = $2 RETURNING *;', [loc, weaponId], (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(201).send(`Moved weapon with id: ${results.rows[0].id}`)
   })
+  client.release();
 }
 
 // suggestion queries
-const getSuggestion = (request, response) => {
-  pool.query('SELECT * FROM suggestions ORDER BY id ASC', (error, results) => {
+const getSuggestion = async (request, response) => {
+  const client = await pool.connect()
+  client.query('SELECT * FROM suggestions ORDER BY id ASC', (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(200).json(results.rows)
   })
+  client.release();
 }
 
-const updateSuggestionMode = (request, response) => {
+const updateSuggestionMode = async (request, response) => {
+  const client = await pool.connect()
   const mode = request.params.mode;
-  pool.query('UPDATE suggestions SET mode = $1 RETURNING *;', [mode], (error, results) => {
+  client.query('UPDATE suggestions SET mode = $1 RETURNING *;', [mode], (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(201).send(`Updated suggestions mode to: ${results.rows[0].mode}`)
   })
+  client.release();
 }
 
-const setSuggestionPlayer = (request, response) => {
+const setSuggestionPlayer = async (request, response) => {
+  const client = await pool.connect()
   const player = request.params.player;
 
-  pool.query('UPDATE suggestions SET player = $1 RETURNING *;', [player], (error, results) => {
+  client.query('UPDATE suggestions SET player = $1 RETURNING *;', [player], (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(201).send(`Updated suggestion`)
   })
+  client.release();
 }
 
-const makeSuggestion = (request, response) => {
+const makeSuggestion = async (request, response) => {
+  const client = await pool.connect()
   const weapon = request.params.weapon;
   const room = request.params.room;
   const person = request.params.person;
 
-  pool.query('UPDATE suggestions SET weapon = $2, room = $3, person = $4, mode = $5 RETURNING *;', [weapon, room, person, 'S'], (error, results) => {
+  client.query('UPDATE suggestions SET weapon = $2, room = $3, person = $4, mode = $5 RETURNING *;', [weapon, room, person, 'S'], (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(201).send(`Updated suggestion`)
   })
+  client.release();
 }
 
 const submitCounter = (request, response) => {
   const counter = request.params.counter;
-  pool.query('UPDATE suggestions SET counter = $1 RETURNING *;', [counter], (error, results) => {
+  client.query('UPDATE suggestions SET counter = $1 RETURNING *;', [counter], (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(201).send(`Updated counter to: ${results.rows[0].counter}`)
   })
 }
 
-const finishSuggestion = (request, response) => {
-  pool.query('UPDATE suggestions SET player = $1 weapon = $2, room = $3, person = $4, mode = $5 RETURNING *;', [null, null, null, null, 'S'], (error, results) => {
+const finishSuggestion = async (request, response) => {
+  const client = await pool.connect()
+  client.query('UPDATE suggestions SET player = $1 weapon = $2, room = $3, person = $4, mode = $5 RETURNING *;', [null, null, null, null, 'S'], (error, results) => {
     if (error) {
+      client.release();
       throw error
     }
     response.status(201).send(`Updated suggestion`)
   })
+  client.release();
 }
 
 
